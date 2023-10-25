3e25f916cba87f5355ccd8b6ddd86d076983d57c
mkawsar <kawsar.diu888@gmail.com>
2023-10-25 09:34:22 +0600

    nuxt project setup with tailwind css

---
+++ client/tailwind.config.js
@@ -0,0 +1,27 @@
+import defaultTheme from 'tailwindcss/defaultTheme';
+
+export default {
+    content: [
+        // `${srcDir}/components/**/*.{vue,js,ts}`,
+        // `${srcDir}/layouts/**/*.vue`,
+        // `${srcDir}/pages/**/*.vue`,
+        // `${srcDir}/composables/**/*.{js,ts}`,
+        // `${srcDir}/plugins/**/*.{js,ts}`,
+        // `${srcDir}/utils/**/*.{js,ts}`,
+        // `${srcDir}/App.{js,ts,vue}`,
+        // `${srcDir}/app.{js,ts,vue}`,
+        // `${srcDir}/Error.{js,ts,vue}`,
+        // `${srcDir}/error.{js,ts,vue}`,
+        // `${srcDir}/app.config.{js,ts}`,
+        "./app.vue"
+    ],
+    theme: {
+        extend: {
+            colors: {
+                primary: defaultTheme.colors.green
+            }
+        },
+    },
+    plugins: [],
+}
+
